b6acbd92554b236bd8c2e13084bc157fc17a88e0
David R <dsrestrepo@unicauca.edu.co>
2024-01-14 16:29:36 +0000

    updating gdelt code

---
+++ datasets/violence_prediction/Internet/gdelt/Bucaramanga.sql
@@ -0,0 +1,15 @@
+-- Codes: http://data.gdeltproject.org/documentation/CAMEO.Manual.1.1b3.pdf
+-- http://efele.net/maps/fips-10/data/fips-all.txt
+SELECT
+  GLOBALEVENTID, SQLDATE, MonthYear, Year, Actor1Name, Actor1CountryCode, Actor2Name, Actor2CountryCode, EventCode, 
+  NumMentions, NumSources, NumArticles, AvgTone, ActionGeo_CountryCode, ActionGeo_ADM1Code, ActionGeo_Lat, ActionGeo_Long, 
+  ActionGeo_FeatureID, SOURCEURL
+FROM
+  `gdelt-bq.full.events`
+WHERE
+  (ActionGeo_CountryCode = 'CO' AND ActionGeo_ADM1Code = 'CO26') AND
+  SQLDATE >= 20150101 AND
+  (EventCode LIKE '18%' OR EventCode LIKE '19%' OR EventCode LIKE '20%') -- AND
+  -- (LOWER(SOURCEURL) LIKE '%violence%' OR LOWER(SOURCEURL) LIKE '%violencia de genero%' OR 
+  --LOWER(SOURCEURL) LIKE '%violencia domestica%' OR LOWER(SOURCEURL) LIKE '%violencia contra la mujer%' OR
+  -- LOWER(SOURCEURL) LIKE '%abuso%' OR LOWER(SOURCEURL) LIKE '%violacion%')
